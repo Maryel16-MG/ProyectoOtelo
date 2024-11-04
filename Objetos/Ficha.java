3faa8f5f4bbb3b2a73d8c6e4a9639dc42944f515
Thyfa <thmartinezgo@est.utn.ac.cr>
2024-11-04 14:26:40 -0600

    Se crea la clase ficha
    Se hacen unas modificaciones en casilla

---
+++ Objetos/Ficha.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+/**
+ *
+ * @author thyfa
+ */
+public class Ficha {
+   private Colores color;
+
+    public Ficha(Colores color) {
+        this.color = color;
+    }
+
+    public Colores getColor() {
+        return color;
+    }
+
+    public void setColor(Colores color) {
+        this.color = color;
+    }
+    
+    
+}
