891c326120f9049e7d303e76dbb090db0fb92e82
Lesly Araya <lesarayamor@est.utn.ac.cr>
2024-11-04 14:40:15 -0600

    Se crea la clase Hilo al paquete Utilizables
    Se programa la clase hilo

---
+++ Utilizables/Hilo.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utilizables;
+
+import static Utilizables.Colores.Negro;
+import static Utilizables.Colores.Rojo;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author user
+ */
+public class Hilo {
+       private JButton btn;
+    private Colores color;
+
+    public Colores getColor() {
+        return color;
+    }
+
+    public void setColor(Colores color) {
+        this.color = color;
+    }
+
+    public JButton getBtn() {
+        return btn;
+    }
+
+    public void setBtn(JButton btn) {
+        this.btn = btn;
+    }
+
+    public void animationBox() {
+        for (int i = 0; i < 4; i++) {
+            if (this.getColor() == Rojo) {
+                btn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/negro" + i + ".png")));
+            }
+            if (this.getColor() == Negro) {
+                btn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/rojo" + i + ".png")));
+            }
+            btn.setContentAreaFilled(false);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+
+            }
+        }
+
+        if (this.getColor() != Rojo) {
+            btn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/negro0" + ".png")));
+
+        }
+        if (this.getColor() != Negro) {
+            btn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/rojo0" + ".png")));
+        }
+    }
+
+    public void run() {
+        animationBox();
+    }
+ 
+}
