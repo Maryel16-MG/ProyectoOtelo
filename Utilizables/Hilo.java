469c9c8123ef9cceb961a92ee32fdb2e1e18700b
Thyfa <thmartinezgo@est.utn.ac.cr>
2024-11-05 20:13:53 -0600

    se realizan algunas correciones de errores

--- Utilizables/Hilo.java
+++ Utilizables/Hilo.java
@@ -62,9 +62,5 @@ public class Hilo {
         animationBox();
     }
 
-    public void start() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 
-   
 }
